782fcc6e4e5b0834850d1146383a52341cef26c5
250541063-blip <250541063@firat.edu.tr>
2025-11-12 20:13:50 +0300

    Update GeometriHesap.java

--- GeometriHesap.java
+++ GeometriHesap.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [FATMA TÜRKMEN ]
+ * Ogrenci No: [250541063]
+ * Tarih: [12.10.2025]
  * Aciklama: Gorev 1 - Geometrik Sekil Hesaplayici
  * 
  * Bu program temel geometrik sekillerin alan ve
